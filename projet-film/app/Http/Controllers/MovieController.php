bdb2a2bfe61da7e60a462192c729bdcf77dc794e
anakynn <anakynn.renoux2003@gmail.com>
2026-03-13 16:02:12 +0100

    second commit

--- projet-film/app/Http/Controllers/MovieController.php
+++ projet-film/app/Http/Controllers/MovieController.php
@@ -11,6 +11,13 @@ use Illuminate\Http\Request;
             ['id' => 1, 'title' => 'la guerre des mondes', 'description' => 'Description du premier film...'],
             ['id' => 2, 'title' => 'blue story', 'description' => 'Description du deuxième film...'],
             ['id' => 3, 'title' => 'Le cinquième élément', 'description' => 'Description du troisième film...'],
+            ['id'=> 4, 'title'=> 'Le dernier survivant', 'description' => 'Description du quatrième film...'],
+            ['id'=> 5, 'title'=> '', 'description' => 'Description du cinquième film...'],
+            ['id'=> 6, 'title'=> '', 'description' => 'Description du sixième film...'],
+            ['id'=> 7, 'title'=> '', 'description' => 'Description du septième film...'],
+            ['id'=> 8, 'title'=> '', 'description' => 'Description du huitième film...'],
+            ['id'=> 9, 'title'=> '', 'description' => 'Description du neuvième film...'],
+            ['id'=> 10, 'title'=> '', 'description' => 'Description du dixième film...'],
         ];
     }
 
