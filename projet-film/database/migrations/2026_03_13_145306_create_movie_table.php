d236691c9e5ec10052fc350adf976877e590526a
Junior EBATA <juniorebata93@gmail.com>
2026-06-03 01:00:42 +0200

    commit7(update de la BDD avec toutes les images)

--- projet-film/database/migrations/2026_03_13_145306_create_movie_table.php
+++ projet-film/database/migrations/2026_03_13_145306_create_movie_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('content');
              $table->string('genre');
-             $table->string('image')->nullable();
+             $table->string('image');
             $table->timestamps();
         });
     }
