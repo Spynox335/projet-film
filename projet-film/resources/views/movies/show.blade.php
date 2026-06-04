269c646bba1bae44af80e92bfb59984a864a88f6
Junior EBATA <juniorebata93@gmail.com>
2026-06-04 16:07:24 +0200

    suppression erreur

--- projet-film/resources/views/movies/show.blade.php
+++ projet-film/resources/views/movies/show.blade.php
@@ -1,15 +1,21 @@
 @extends('layout.app')
 
 @section('content')
-    
-    <h1>{{ $movie->title }}</h1>
+      
+  <header>
+      <a href="/movies">Acceuil</a>
+  </header>   
+  <article class="article">
+      <h1>{{ $movie->title }}</h1>
+      <img src="{{ $movie->image }} " alt="Affiche de {{ $movie->title }}" class="image">
+  </article>
+  <article class="desc">
       <p>{{ $movie->content }}</p>
-
-    <h3>Genre : {{ $movie->genre }}</h3>
-
-    <img src="{{ $movie->image }}" alt="Affiche de {{ $movie->image}}">
+      <h3>Genre : {{ $movie->genre }}</h3>
+  </article>
 
   <br>
+
  <a href="/movies">Retour</a>
      <button class="btn-create">
         <a href="/movies/{{ $movie['id']}}/edit">Mettre a jour</a>
